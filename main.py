f94df132488a2c64a40891b723d63d01ec9dfdca
Weber Teixeira <107162356+Binhopro@users.noreply.github.com>
2022-06-10 01:34:23 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+print("="*50)
+print(" -------- CALCULAODRA --------")
+print ("Só dijite valoes numercios ")
+n1 = int(input("digite um valor: "))
+n2 = int(input("digite um valor: "))
+s = n1 + n2
+s1 = n1 - n2
+s2 = n1 * n2
+s3 = n1 / n2
+s4 = n1 ** n2
+s5 = n1 // n2
+s6 = n1 % n2
+
+print("")
+print("A soma entre {} e {} é igual a {}".format(n1, n2, s))
+print("A sub entre {} e {} é igual a {}".format(n1, n2, s1))
+print("A mult entre {} e {} é igual a {}".format(n1, n2, s2))
+print("A div entre {} e {} é igual a {}".format(n1, n2, s3))
+print("A elevado entre {} e {} é igual a {}".format(n1, n2, s4))
+print("O nº inteiro da div entre {} e {} é igual a {}".format(n1, n2, s5))
+print("O nº do resto da div entre {} e {} é igual a {}".format(n1, n2, s6))
+print("")
+print(" ---------- FIM DA CALCULAORA ----------")
+print("="*50)
+print(" -------- CALCULAODRA --------")
+print ("Só dijite valoes numercios ")
+n1 = int(input("digite um valor: "))
+n2 = int(input("digite um valor: "))
+s = n1 + n2
+s1 = n1 - n2
+s2 = n1 * n2
+s3 = n1 / n2
+s4 = n1 ** n2
+s5 = n1 // n2
+s6 = n1 % n2
+print("")
+print("A soma é igual a = {}".format( s))
+print("A subtração é igual a = {}".format( s1))
+print("A multiplicação é igual a = {}".format( s2))
+print("A divisão é igual a = {} ".format( s3))
+print("O nº elevado é igual a = {} ".format( s4))
+print("O nº inteiro da div entre  é igual a = {} ".format( s5))
+print("O nº do resto da divisão é igual a = {} ".format( s6))
+print("")
+print(" ---------- FIM DA CALCULAORA ----------")
+print("="*50)
